16195c09dfb023ac097d1b27e74386ddc08d8682
Constantin Zborowska <constantin.zborowska@alumni.fh-aachen.de>
2021-10-25 22:23:05 +0200

    create new angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderComponent } from './components/header/header.component';
+import { DeviceListComponent } from './components/device-list/device-list.component';
+import { DeviceComponent } from './components/device/device.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DeviceListComponent,
+    DeviceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
