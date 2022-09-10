7801b4ccae81ab60a496640f0976c3f253e97632
Constantin Zborowska <constantin.zborowska@alumni.fh-aachen.de>
2022-09-10 12:40:10 +0200

    implement program summary view

--- src/app/components/program/program.component.ts
+++ src/app/components/program/program.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Program } from "../../interfaces/Summaries";
+import { faCrown } from "@fortawesome/free-solid-svg-icons";
+import { getProgramName, calculateEnergyCosts, calculateDateRange } from '../template.functions';
 
 @Component({
   selector: '[program-row]',
@@ -8,6 +10,12 @@ import { Program } from "../../interfaces/Summaries";
 })
 export class ProgramComponent implements OnInit {
   @Input() program!: Program;
+
+  public getProgramName = getProgramName;
+  public calculateEnergyCosts = calculateEnergyCosts;
+  public calculateDateRange = calculateDateRange;
+
+  faCrown = faCrown;
 
   constructor() { }
 
--- src/app/interfaces/Summaries.ts
+++ src/app/interfaces/Summaries.ts
@@ -10,6 +10,7 @@ export interface Program {
   program_id: number;
   program_counter: number;
   program_last_run: number;
+  program_is_king: boolean;
   program_runtime_average: number;
   program_runtime_stdev: number;
   program_estimated_average: number;
