f69bb42694d925ab6226f91530eb651b43921895
Constantin Zborowska <constantin.zborowska@alumni.fh-aachen.de>
2022-09-23 19:54:06 +0200

    implement heart beat animation on data reload

--- src/app/components/run/run.component.ts
+++ src/app/components/run/run.component.ts
@@ -2,11 +2,23 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Run } from '../../interfaces/Run';
 import { faTrash, faCircleExclamation, faHeartPulse } from "@fortawesome/free-solid-svg-icons";
 import { getProgramName, getSequenceName, calculateDateRange, calculateTimePrediction, calculateEnergyCosts } from '../template.functions';
+import { trigger, state, style, transition, animate } from "@angular/animations";
 
 @Component({
   selector: '[run-table-row]',
   templateUrl: './run.component.html',
-  styleUrls: ['./run.component.css']
+  styleUrls: ['./run.component.css'],
+  animations: [
+    trigger('heartBeatTrigger', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.5s', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('0.5s', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class RunComponent implements OnInit {
   public getProgramName = getProgramName;
